17e35774f9375ab4914954d2bc1cdfe23a371dde
erikwastaken <46598911+erikwastaken@users.noreply.github.com>
2022-01-18 11:47:04 +0100

    Tell zsh to not reserve space for escape sequences

--- include/zsh_formatter.hpp
+++ include/zsh_formatter.hpp
@@ -6,10 +6,10 @@
 class ZshFormatter : public Formatter {
     public:
         ZshFormatter() = default;
-        virtual std::string boldOn() const override { return "\033[1m"; };
-        virtual std::string fgColor(int color) const override { return "\033[38;5;" + std::to_string(color) + "m"; };
-        virtual std::string bgColor(int color) const override { return "\033[48;5;" + std::to_string(color) + "m"; };
-        virtual std::string reset() const override { return "\033[0m"; };
+        virtual std::string boldOn() const override { return "%{\033[1m%}"; };
+        virtual std::string fgColor(int color) const override { return "%{\033[38;5;" + std::to_string(color) + "m%}"; };
+        virtual std::string bgColor(int color) const override { return "%{\033[48;5;" + std::to_string(color) + "m%}"; };
+        virtual std::string reset() const override { return "%{\033[0m%}"; };
 
         virtual std::string arrow_start(int fg, int bg) const override {
             return fgColor(fg) + bgColor(bg) + boldOn() + " ";
