feffdbeab21ce4362af03abdf6c897d7f8258dab
erikwastaken <46598911+erikwastaken@users.noreply.github.com>
2022-02-24 22:45:43 +0100

    Add lock symbol in ssh segment

--- src/ssh_host_segment.cpp
+++ src/ssh_host_segment.cpp
@@ -2,8 +2,11 @@
 
 
 SshHostSegment::SshHostSegment(int fg, int bg) : Segment(fg,bg) {
-    if (auto host = std::getenv("SSH_CLIENT"))
-        _hostName = host;
+    if (auto host = std::getenv("SSH_CLIENT")) {
+        _hostName += "\ue0a2";
+        _hostName += " ";
+        _hostName += host;
+    }
 }
 
 std::string SshHostSegment::get() const {
