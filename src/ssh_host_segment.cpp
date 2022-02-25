3b6677ef55b3c698eadfcba7aae15ecc39deaf4e
erikwastaken <46598911+erikwastaken@users.noreply.github.com>
2022-02-25 15:59:10 +0100

    Display padlock symbol before hostname
    commit 51c1b300fd03e7bbb031167f015c892a92a7ba24
    Author: erikwastaken <46598911+erikwastaken@users.noreply.github.com>
    Date:   Fri Feb 25 15:58:49 2022 +0100
    
        Properly display padlock symbol

--- src/ssh_host_segment.cpp
+++ src/ssh_host_segment.cpp
@@ -3,6 +3,12 @@
 
 SshHostSegment::SshHostSegment(int fg, int bg, Formatter* fmt) : Segment(fg,bg) {
     if (std::getenv("SSH_CLIENT")) {
+        // reset bold to properly display padlock glyph
+        _hostName += fmt->reset();
+        _hostName += fmt->fgColor(fg);
+        _hostName += fmt->bgColor(bg);
+        _hostName += "\ue0a2 ";
+        _hostName += fmt->boldOn();
         _hostName += fmt->hostname();
     }
 }
